d46cd7b725e6c3fc24f13c0e69bee5dd02a2ffaa
Bazinga <850462618@qq.com>
2020-11-28 18:25:18 +0800

    初步实现GUI逻辑

--- Client/Client.py
+++ Client/Client.py
@@ -1,10 +1,16 @@
 # @Author : ZZJ
-
 from Client.Conn import Conn
+from Client.GUI import *
 
 class Client:
     """客户端逻辑，调用GUI模块，显示界面"""
     def __init__(self):
         self.conn = Conn()
+
+    def start(self):
+        """启动客户端程序。按照流程图过程实现。"""
+        #TODO 逻辑在GUI里写还是在这里写？
+        pass
+
 
 
